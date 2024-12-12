9bb02672caf2b9660dafbeba75c591c94ccae2b3
root <root@TszChoiWare>
2024-12-12 12:56:57 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,14 @@ int main() {
   std::cerr << std::unitbuf;
 
   std::cout << "$ ";
-  
   std::string input;
+  while (true)
+  {
+    
   std::getline(std::cin, input);
   std::cout << input << ": not found" << std::endl;
+  std::cout << "$ ";
+
+  }
+  
 }
