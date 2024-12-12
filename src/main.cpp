61a18ac704960b2822235fd31553f1134a414fb3
root <root@TszChoiWare>
2024-12-13 02:43:27 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <filesystem>
+
+// Function to split a string by a delimiter
+std::vector<std::string> split(const std::string& str, char delimiter) {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream iss(str);
+
+    // Extract tokens separated by the delimiter
+    while (std::getline(iss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
 
 int main() {
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  for (;;) {
+  while (true) {
     // Print the shell prompt
     std::cout << "$ ";
 
@@ -19,33 +35,48 @@ int main() {
       return 0;
     }
 
-    // Handle the "echo ..."
+    // Handle the "echo ..." command
     if (input.find("echo ") == 0) {
       const int ECHO_LEN = 5;
       std::cout << input.substr(ECHO_LEN) << std::endl;
-    } 
-    // Handle the "type ..."
+    }
+
+    // Handle the "type ..." command
     else if (input.find("type ") == 0) {
       const int TYPE_LEN = 5;
+      bool found = false;
       std::string argument = input.substr(TYPE_LEN);
 
-      if (argument == "echo") {
-        std::cout << "echo is a shell builtin" << std::endl;
+      if(argument == "type" || argument == "exit" || argument == "echo"){
+        found = true;
+        std::cout << argument << " is a shell builtin" << std::endl;
       }
-      else if (argument == "exit") {
-        std::cout << "exit is a shell builtin" << std::endl;
-      }
-      else if (argument == "type") {
-        std::cout << "type is a shell builtin" << std::endl;
+      // Search for the command in PATH directires
+      else {
+        std::string pathEnv = std::getenv("PATH");
+        std::vector<std::string> directories = split(pathEnv, ':');
+
+        for (const std::string& dir : directories) {
+          std::string fullPath = dir + "/" + argument;
+          
+          if (std::filesystem::exists(fullPath)) {
+            std::cout << argument << " is " << fullPath << std::endl;
+            found = true;
+            break;
+          }
+        }
       }
 
-       else {
-        std::cout << argument << ": not found" << std::endl;
-       }
+      // If the command was not found
+      if (!found) {
+          std::cout << argument << ": not found" << std::endl;
+      }
     }
-    // If the argument is not recognized
+
     else {
       std::cout << input << ": command not found" << std::endl;
     }
   }
+
+  return 1;
 }
