2f32454d62f48ffefcaa765cdf53965d921b3145
root <root@TszChoiWare>
2024-12-12 13:16:29 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+#include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std::cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
-  std::string input;
-  while (true)
-  {
-    
-  std::getline(std::cin, input);
+    for (;;) {
+        std::cout << "$ ";
+        std::string input;
+        std::getline(std::cin, input);
 
-  if (input == "exit 0")
-    return 0;
-  
-  std::cout << input << ": not found" << std::endl;
-  std::cout << "$ ";
+        if (input == "exit 0") {
+            return 0;
+        }
 
-  }
-  
+        if (input.rfind("echo ", 0) == 0) {
+            std::cout << input.substr(5) << std::endl;
+        } else {
+            std::cout << input << ": command not found" << std::endl;
+        }
+    }
 }
